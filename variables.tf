c8548a82f8b8e5886e916d6525addf725e9db362
chengyh817 <49397065+chengyh817@users.noreply.github.com>
2024-07-09 18:04:02 +0800

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -2,3 +2,13 @@ variable "vpc_cidr" {
     type = string
     default =  "10.0.0.0/16"
 }
+
+variable "ALICLOUD_ACCESS_KEY" {
+  description = "RAM user access_key"
+  sensitive   = true
+}
+
+variable "ALICLOUD_SECRET_KEY" {
+  description = "RAM user secret_key"
+  sensitive   = true
+}
